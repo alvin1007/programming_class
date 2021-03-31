d0367698316128465ec78afd7d05bb38b10975c7
Young Jea Cho <77112874+alvin1007@users.noreply.github.com>
2021-03-31 17:53:42 +0900

    Create assignment3.c

---
+++ assignment3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+	int num[11], i, j, temp;
+	for (i = 0; i < 10; i++)
+	{
+         scanf("%d", &num[i]);
+	}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (num[i] < num[j])
+			{
+				temp = num[j];
+				num[j] = num[i];
+				num[i] = temp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", num[i]);
+	}
+	return 0;
+}
